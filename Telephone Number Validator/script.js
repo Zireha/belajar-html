4ec6558ac88b220e2ce96add8ec5071684d73f70
Zireha <cheva.wahyu@gmail.com>
2024-07-31 22:00:29 +0700

    fixed US telephone number validation

--- Telephone Number Validator/script.js
+++ Telephone Number Validator/script.js
@@ -1,12 +1,33 @@
 const phoneRegex = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/
-// const numInput = document.getElementById("num_input").value
+const numInput = document.getElementById("user-input")
+const checkBtn = document.getElementById("check-btn")
+const outputContainer = document.getElementById("results-div")
+const clearBtn = document.getElementById("clear-btn") // Diperbaiki dari getElementById9
 
-function isValidNumber(number){
-    if(phoneRegex.test(number) == true){
+function isValidNumber(number) {
+    if (phoneRegex.test(number)) {
         return `Valid US number: ${number}`
     } else {
         return `Invalid US number: ${number}`
     }
 }
 
-console.log(isValidNumber("1(555)555-5555"))
+function appendResult(num) {
+    const element = document.createElement("p");
+    element.textContent = isValidNumber(num);
+    outputContainer.appendChild(element)
+}
+
+checkBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (numInput.value === "") {
+        alert("Please provide a phone number");
+    } else {
+        appendResult(numInput.value)
+        numInput.value = ""
+    }
+})
+
+clearBtn.addEventListener('click', (e) => {
+    outputContainer.textContent = ""
+})
